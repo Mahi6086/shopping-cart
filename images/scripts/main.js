92641e6fb47654bfa724a192504c53539ca3d865
Muaz55 <bangladesh@amerdesh.com>
2021-08-22 10:41:19 +0600

    first commit

---
+++ images/scripts/main.js
@@ -0,0 +1,7 @@
+/* const div1 = document.getElementById('first');
+const div2 = document.getElementById('second');
+const div3 = document.getElementById('third');
+//add event listener
+div1.addEventListener('click', function() {
+    console.log('img1');
+}) */
